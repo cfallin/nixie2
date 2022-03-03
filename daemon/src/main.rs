33db44320e41f9b17a169b207b7040590d41e757
Chris Fallin <chris@cfallin.org>
2022-03-03 10:21:52 -0800

    fix time divergence once and for all: daemon updates clock time over serial continuously

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,16 +1,5 @@
 use chrono::{DateTime, Local};
-use std::time::SystemTime;
-
-fn sleep_until(time: SystemTime) {
-    loop {
-        let now = SystemTime::now();
-        if now >= time {
-            break;
-        }
-        let remaining = time.duration_since(now).unwrap();
-        std::thread::sleep(remaining);
-    }
-}
+use std::time::{Duration, SystemTime};
 
 fn main() {
     let port_name = std::env::var("CLOCK_SERIAL_PORT").unwrap_or("/dev/ttyUSB0".into());
@@ -20,11 +9,9 @@ fn main() {
         .expect("Could not open port");
 
     loop {
-        write!(port, "\n").unwrap();
-        let next_sec = SystemTime::now() + std::time::Duration::from_secs(1);
-        let local: DateTime<Local> = DateTime::from(next_sec);
+        let local: DateTime<Local> = DateTime::from(SystemTime::now());
         let local = local.time().format("%H%M%S").to_string();
-        write!(port, "{}", local).unwrap();
-        sleep_until(next_sec);
+        writeln!(port, "{}", local).unwrap();
+        std::thread::sleep(Duration::from_millis(50));
     }
 }
